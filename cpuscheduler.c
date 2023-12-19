f0cb68a3a673382caec20d3f7f7fa5c8580a24f9
Qudus <adewalequdus7@gmail.com>
2023-12-19 23:14:45 +0200

    Updating the menu to function as required

--- cpuscheduler.c
+++ cpuscheduler.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 FILE *fptr;
 FILE *fptr2;
@@ -32,8 +33,11 @@ void AppendFile(const char *OutputPath){
 */
 int main()
 {
-   char sc_method[]={'NONE','FCFS','SJF','PS','RRS'};
-   char mode[]={'ON', 'OFF'};
+   char *sc_method[]={"NONE","FCFS","SJF","PS","RRS"};
+   char *mode[]={"OFF", "ON"};
+   int option_choice;
+   int sch_choice;
+   int mode_option;
 
    char method_choice[] = "NONE";
    char mode_choice[] = "OFF";
@@ -44,6 +48,88 @@ int main()
       printf("3  |  SHOW RESULT\n");
       printf("4  |  END PROGRAM\n\n");
       printf("OPTION >> ");
+      scanf("%d", &option_choice);
+
+      switch(option_choice){
+         case 1:
+            printf("SCHEDULING METHOD\n\n");
+            printf("1  |  NONE\n");
+            printf("2  |  First Come First Serve\n");
+            printf("3  |  Shortest Job First\n");
+            printf("4  |  Priority Scheduling\n");
+            printf("5  |  Round Robin Scheduling\n\n");
+            printf("OPTION >> ");
+            scanf("%d", &sch_choice);
+
+            switch(sch_choice){
+               case 1:
+                  strcpy(method_choice,sc_method[0]);
+                  printf("%s", method_choice);
+                  break;
+
+               case 2:
+                  strcpy(method_choice,sc_method[1]);
+                  printf("%s", method_choice);
+                  break;
+
+               case 3:
+                  strcpy(method_choice,sc_method[2]);
+                  printf("%s", method_choice);
+                  break;
+
+               case 4:
+                  strcpy(method_choice,sc_method[3]);
+                  printf("%s", method_choice);
+                  break;
+
+               case 5:
+                  strcpy(method_choice,sc_method[4]);
+                  printf("%s", method_choice);
+                  break;
+
+               default:
+                  printf("Please select the correct option");
+            }
+            break;
+
+         case 2: 
+            printf("PREEMPTIVE MODE\n\n");
+            printf("1  |  OFF\n");
+            printf("2  |  ON\n\n");
+            printf("OPTION >> ");
+            scanf("%d", &mode_option);
+
+            switch (mode_option){
+               case 1:
+                  strcpy(mode_choice,mode[0]);
+                  printf("%s", mode_choice);
+                  break;
+
+               case 2:
+                  strcpy(mode_choice,mode[1]);
+                  printf("%s", mode_choice);
+                  break;
+
+               default:
+                  printf("Please put in the correct option");
+               break;
+            
+            }
+            
+            break;
+
+         case 3:
+            printf("Show result was selected");
+            break;
+
+         case 4:
+            printf("End program was selected");
+            break;
+
+            default:
+            printf("Please select the correct option");
+            break;
+      }
 
 
 
